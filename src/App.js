1eb12f9cf5d149126c1cfbfbac9714df721afd73
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-10-30 10:44:52 +0700

    fix responsive issues

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
     <Store>
       <div className="app">
         <div className="app-header">
-          <h1 className="text-purple-gradient text-5xl font-bold">Markdown Previewer</h1>
+          <h1 className="text-purple-gradient text-center text-3xl md:text-5xl font-bold">Markdown Previewer</h1>
           <GithubLink url="https://github.com/harmonify/markdown-previewer" />
         </div>
 
-        <div className="w-5/12">
+        <div className="w-11/12 md:w-9/12 lg:w-6/12">
           <EditorCard />
         </div>
 
-        <div className="w-7/12">
+        <div className="w-11/12 md:w-11/12 lg:w-8/12">
           <PreviewerCard />
         </div>
       </div>
